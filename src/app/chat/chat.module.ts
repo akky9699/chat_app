307ee7a270a5fbf25493fc0f748df5c4e115756c
akky9699 <akshayakky8828@gmail.com>
2019-12-20 22:11:23 +0530

    update

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 import { ChatboxComponent } from './chatbox/chatbox.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { ChatRouteGuardService } from './chat-route-guard.service';
+import { SharedModule } from '../shared/shared.module';
+import { RemoveSpecialCharPipe } from '../shared/pipe/remove-special-char.pipe';
 
 
 
@@ -11,10 +15,11 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     BrowserAnimationsModule,
     RouterModule.forChild([ 
-      { path: 'chat', component: ChatboxComponent}
-    ])
-      ],
-  declarations: [ChatboxComponent]
-  // providers:[ChatRouteGuardService]
+      { path: 'chat', component: ChatboxComponent,canActivate:[ChatRouteGuardService] }
+    ]),
+    SharedModule
+  ],
+  declarations: [ChatboxComponent,RemoveSpecialCharPipe],
+  providers:[ChatRouteGuardService]
 })
 export class ChatModule { }
